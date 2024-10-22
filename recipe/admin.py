23d00f333e5f11a9429c430d1112a9bcdb4b1e7e
juneshone <shamrova.julia@mail.ru>
2024-10-22 18:28:13 +0300

    Add models for recipe app

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipe.models import Ingredient, Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 5
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_display = ('title',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ('dish',)
+    list_display = ('dish', 'price',)
+    raw_id_fields = ('ingredients',)
+    inlines = (RecipeIngredientInline,)
--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    title = models.CharField(
+        'Название ингредиента',
+        max_length=200
+    )
+    calorie = models.IntegerField(
+        'Калорийность ингредиента'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ['title']
+
+
+class Recipe(models.Model):
+    dish = models.CharField(
+        'Название блюда', max_length=100
+    )
+    image = models.ImageField(
+        'Изображение',
+        upload_to='image',
+        blank=True
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        related_name="recipes",
+        verbose_name='Ингредиенты'
+    )
+    price = models.DecimalField(
+        'Стоимость блюда',
+        max_digits=10,
+        decimal_places=2,
+    )
+    instruction = models.TextField(
+        'Инструкция приготовления'
+    )
+
+    def __str__(self):
+        return self.dish
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['dish']
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField(
+        'Количество ингредиента в рецепте'
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=20,
+        default='грамм'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент рецепта'
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецепта'
