553f8e98ed3363524afb34f3bd1728a72df7c743
juneshone <shamrova.julia@mail.ru>
2024-10-23 00:36:57 +0300

    Add view of recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('card1/', views.card1, name='card1'),
-    path('card2/', views.card2, name='card2'),
-    path('card3/', views.card3, name='card3'),
+    path(f'card<int:recipe_num>/', views.card, name='card'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from recipe.models import Recipe, RecipeIngredient
 
 
-def card1(request):
-    return render(request, 'card1.html')
-
-
-def card2(request):
-    return render(request, 'card2.html')
-
-
-def card3(request):
-    return render(request, 'card3.html')
-
+def card(request, recipe_num):
+    # TODO: привязать рецепты к меню
+    try:
+        recipe = get_object_or_404(Recipe, id=recipe_num)
+        recipe_ingredients = RecipeIngredient.objects.filter(recipe=recipe).select_related('ingredient')
+        recipe_context = {
+            'dish': recipe.dish,
+            'image': recipe.image,
+            'price': recipe.price,
+            'instruction': recipe.instruction,
+            'ingredients': recipe_ingredients,
+            'calories': sum([i.ingredient.calorie for i in recipe_ingredients])
+        }
+        return render(request, 'card.html', context={'recipe': recipe_context})
+    except:
+        return render(request, 'card.html', context={
+                'not_recipe': f'Рецепта с id={recipe_num} не найдено'
+            }
+        )
