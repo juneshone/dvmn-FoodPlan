fdfb9c7732c695ad9cd116c189b111b7549dcfc1
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-10-26 20:55:45 +0300

    Change serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,38 +1,15 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
-from .models import Order
+from .models import Subscription
 
 
-# class OrderAllergySerializer(ModelSerializer):
-#
-#     class Meta:
-#         model = OrderAllergy
-#         fields = ['allergy_title']
-
-class OrderSerializer(ModelSerializer):
-    # allergies = OrderAllergySerializer(many=True, allow_empty=False, write_only=True)
+class SubscriptionSerializer(ModelSerializer):
 
     class Meta:
-        model = Order
-        fields = ['id', 'menu_title', 'datestart', 'period', 'user', 'persons', 'breakfast',
+        model = Subscription
+        fields = ['id', 'menu', 'datestart', 'period', 'persons', 'breakfast',
                   'lunch', 'dinner', 'dessert', 'promocode', 'allergy1', 'allergy1', 'allergy2',
                   'allergy3', 'allergy4', 'allergy5', 'allergy6']
 
 
-    def create(self, validated_data):
-        print(validated_data)
-        # allergies = validated_data.pop('allergies')
-        #
-        # order = Order.objects.create(**validated_data)
-        #
-        # for allergy in allergies:
-        #     try:
-        #         allergy = Allergy.objects.get(title=allergy['title'])
-        #     except Allergy.DoesNotExist:
-        #         raise ValidationError(f"Аллергия с названием {allergy['title']} не найдена.")
-        #
-        #     OrderAllergy.objects.create(order=order, allergy=allergy)
-        #
-        order = Order.objects.create(**validated_data)
-        return order
