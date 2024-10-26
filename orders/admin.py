0920fe4b0d8327fac61eccb475488bb959d8b523
juneshone <shamrova.julia@mail.ru>
2024-10-26 22:02:51 +0300

    Add changes to torder display  in admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -5,8 +5,16 @@ from .models import Order, Menu
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    search_fields = ('user',)
     list_display = [
+        'user',
         'menu',
+        'breakfast',
+        'lunch',
+        'dinner',
+        'dessert',
+        'persons',
+        'datestart',
     ]
 
 
