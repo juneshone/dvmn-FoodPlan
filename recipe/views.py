9c26d77ef021eb98dd90a5edb987776eff4d9db8
juneshone <shamrova.julia@mail.ru>
2024-10-28 14:40:05 +0300

    Added conditions for viewing recipe

--- recipe/views.py
+++ recipe/views.py
@@ -2,10 +2,21 @@ import random
 
 from django.shortcuts import render, get_object_or_404
 from recipe.models import Recipe, RecipeIngredient
+from orders.models import Order
 
 
 def card(request):
-    recipe = get_object_or_404(Recipe, id=1)
+    user = request.user
+    if user.id:
+            order = Order.objects.filter(user=user.id, payment_status='PAID').last()
+            if order:
+                menu = order.menu.foodtype
+                recipe = random.choice(Recipe.objects.filter(foodtype=menu).prefetch_related('ingredients'))
+            else:
+                recipe = get_object_or_404(Recipe, id=1)
+    else:
+        recipe = get_object_or_404(Recipe, id=1)
+
     recipe_ingredients = RecipeIngredient.objects.filter(recipe=recipe).select_related('ingredient')
     recipe_context = {
         'dish': recipe.dish,
